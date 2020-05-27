2f47d4679c10248ec4d1d22aeb186f0e863ef3e3
7UsmanS <59475843+7UsmanS@users.noreply.github.com>
2020-05-27 12:34:41 -0700

    Add existing file

---
+++ Java-TetrisTest-master/src/unitTests/AllTests.java
@@ -0,0 +1,11 @@
+package unitTests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ BlockGridTest.class, TetrisBlockTest.class, TetrisGridTest.class })
+public class AllTests {
+
+}
